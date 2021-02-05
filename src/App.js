503ea701e5817fe43099530c63540d0a6a2e9d2d
Ginger Alford origins <Ginger@Gingers-MacBook-Pro.local>
2021-02-05 01:08:54 -0500

    finished Workout Log

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Sitebar from './home/Navbar';
+import Auth from './auth/Auth';
+import WorkoutIndex from './workouts/WorkoutIndex';
 
 function App() {
+  const [sessionToken, setSessionToken] = useState('');
+  //using useState Hook to create a new state variable, sessionToken, will be empty, then have value, then emptied again
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, [])
+  //only runs on initial component load, because dependency is set to nothing. Just sets token to the localStorage token, if it exists
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+  //this takes a token (from API call) and sets it in both localStorage and in the variable.
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
+
+  const protectedViews = () => {
+    return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken} /> : <Auth updateToken={updateToken} /> )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sitebar clickLogout={clearToken} />
+      {/* <Auth updateToken={updateToken} /> */}
+      {protectedViews()}
     </div>
   );
 }
